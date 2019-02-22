c9d0eff9b2d3dd7d28938ba266937ff61d23adb4
Yelken Gonzales <yelkezin@gmail.com>
2019-02-22 01:48:55 -0300

    Primeiro commit

---
+++ HELP.md
@@ -0,0 +1,7 @@
+# JUG PE
+# Palestra sobre implementação do Spring Security
+
+### Guides
+
+* [Securing a Web Application](https://spring.io/guides/gs/securing-web/)
+* [Spring Boot and OAuth2](https://spring.io/guides/tutorials/spring-boot-oauth2/)
---
+++ src/main/java/br/com/jugpe/JugpeApplication.java
@@ -0,0 +1,13 @@
+package br.com.jugpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JugpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JugpeApplication.class, args);
+	}
+
+}
